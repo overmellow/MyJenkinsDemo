d39fc5f930567faa7e99f81331634ece8eefd701
overmellow <morteza_faraji@yahoo.com>
2022-05-17 10:47:12 -0700

    second commit

---
+++ src/test/java/com/wv/mfaraji/spring/mockmvcdemo/MyJenkinsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wv.mfaraji.spring.mockmvcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJenkinsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
